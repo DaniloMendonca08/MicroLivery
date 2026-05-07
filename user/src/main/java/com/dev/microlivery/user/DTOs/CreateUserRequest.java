200a6a676f5b24d54bf18121dc73101f36f74b5f
Danilo Mendonça <danilomendonca08@gmail.com>
2026-05-06 21:44:25 -0300

    feat: add user request dto

---
+++ user/src/main/java/com/dev/microlivery/user/DTOs/CreateUserRequest.java
@@ -0,0 +1,20 @@
+package com.dev.microlivery.user.DTOs;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserRequest(
+
+        @NotBlank(message = "Name cannot be blank")
+        String name,
+
+        @NotBlank(message = "Email cannot be blank")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Password cannot be blank")
+        @Size(min = 8, message = "Password must have at least 8 characters")
+        String password
+        
+) {}
